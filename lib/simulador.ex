033b8fc5f498cf6cc5fc17b36305c542709dceb6
fep <franpaez@gmail.com>
2025-08-27 10:55:13 -0300

    Elimina función privada comprar() del Simulador.

--- lib/simulador.ex
+++ lib/simulador.ex
@@ -1,13 +1,9 @@
 defmodule Simulador do
 
-  defp comprar(id_producto, forma_de_envio, medio_de_pago) do
-    Libremarket.Ui.comprar(id_producto, forma_de_envio, medio_de_pago)
-  end
-
   def simular_compra() do
     envio = Enum.random([:retira, :correo])
     pago = Enum.random([:efectivo, :transferencia, :td, :tc])
-    comprar(:rand.uniform(10), envio, pago)
+    Libremarket.Ui.comprar(:rand.uniform(10), envio, pago)
   end
 
   def simular_compras_secuencial(cantidad \\ 1) do
