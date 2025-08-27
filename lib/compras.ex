d96de84f759e8b30e6a90f32333c4fb2ffc744d5
fep <franpaez@gmail.com>
2025-08-27 13:30:49 -0300

    Modifica el parámetro por defecto a un mapa.

--- lib/compras.ex
+++ lib/compras.ex
@@ -18,7 +18,7 @@ defmodule Libremarket.Compras.Server do
   @doc """
   Crea un nuevo servidor de Compras
   """
-  def start_link(opts \\ []) do
+  def start_link(opts \\ %{}) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
