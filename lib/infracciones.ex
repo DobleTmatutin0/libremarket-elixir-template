3a4d21fc0632f486d0c0632d56eab0974df5e8ee
fep <franpaez@gmail.com>
2025-09-13 02:42:23 -0300

    Agrega template para servidor de Infracciones.

---
+++ lib/infracciones.ex
@@ -0,0 +1,58 @@
+defmodule Libremarket.Infracciones do
+
+  def detectar_infraccion() do
+    Enum.random([:true, :false])
+  end
+
+end
+
+defmodule Libremarket.Infracciones.Server do
+  @moduledoc """
+  Compras
+  """
+
+  use GenServer
+
+  # API del cliente
+
+  @doc """
+  Crea un nuevo servidor de Compras
+  """
+  def start_link(opts \\ %{}) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  def detectar_infraccion(pid \\ __MODULE__, id_compra) do
+    GenServer.call(pid, {:detectar_infraccion, id_compra})
+  end
+
+  def listar_infracciones(pid \\ __MODULE__) do
+    GenServer.call(pid, :listar_infracciones)
+  end
+
+  # Callbacks
+
+  @doc """
+  Inicializa el estado del servidor
+  """
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @doc """
+  Callback para un call :comprar
+  """
+  @impl true
+  def handle_call({:detectar_infraccion, id_compra}, _from, state) do
+    result = Libremarket.Infracciones.detectar_infraccion
+    new_state = Map.put(state, id_compra, result)
+    {:reply, result, new_state}
+  end
+
+  @impl true
+  def handle_call(:listar_infracciones, _from, state) do
+    {:reply, state, state}
+  end
+
+end
