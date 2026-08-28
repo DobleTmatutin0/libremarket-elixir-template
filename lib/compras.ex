c1ea90d395a881ceb0270d35ec4f7b252e35d977
DobleTmatutin0 <zz_00024@hotmail.com>
2026-08-28 08:13:11 -0300

    add list of products and list product method

--- lib/compras.ex
+++ lib/compras.ex
@@ -8,7 +8,7 @@ end
 
 defmodule Libremarket.Compras.Server do
   @moduledoc """
-  Compras
+    Compras
   """
 
   use GenServer
@@ -16,7 +16,7 @@ defmodule Libremarket.Compras.Server do
   # API del cliente
 
   @doc """
-  Crea un nuevo servidor de Compras
+    Crea un nuevo servidor de Compras
   """
   def start_link(opts \\ %{}) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -26,23 +26,44 @@ defmodule Libremarket.Compras.Server do
     GenServer.call(pid, :comprar)
   end
 
+  def listar_productos(pid \\ __MODULE__) do
+    GenServer.call(pid, :listar_productos)
+  end
+
   # Callbacks
 
   @doc """
-  Inicializa el estado del servidor
+    Inicializa el estado del servidor
   """
   @impl true
-  def init(state) do
-    {:ok, state}
+  def init(_state) do
+    {
+      :ok,
+      %{
+        productos: [
+          %{id: 1, nombre: "Notebook"},
+          %{id: 2, nombre: "Mouse"},
+          %{id: 3, nombre: "Keyboard"},
+        ]
+      }
+    }
   end
 
   @doc """
-  Callback para un call :comprar
+    Callback para un call :comprar
   """
   @impl true
   def handle_call(:comprar, _from, state) do
     result = Libremarket.Compras.comprar
     {:reply, result, state}
+  end
+
+  @doc """
+    Callback para un call :listar_productos
+  """
+  @impl true
+  def handle_call(:listar_productos, _from, state) do
+    {:reply, state.productos, state}
   end
 
 end
